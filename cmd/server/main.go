ac1eb3f04b9ac6be4cdc9689ab597ef4cffaef4e
aghabay <mahammadali.aghabayli.r@gmail.com>
2022-05-27 23:49:25 +0400

    Creating routes and handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/aghabay/goapi/internal/transport/http"
 )
 
+// App - the struct which contains things like pointers
+// to database connections
 type App struct{}
 
-func (app *App) Run() error{
-	fmt.Println("Setting Up Our App")
+// Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting Up Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
 	return nil
 }
 
 func main() {
-	fmt.Println("REST API")
+	fmt.Println("Go REST API Course")
 	app := App{}
-	if err := app.Run(); err != nil{
+	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up our REST API")
 		fmt.Println(err)
 	}
